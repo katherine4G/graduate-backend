197f4ec7fbdfb91e459e9186995e4bc48bf6190d
katherine4G <112660945+katherine4G@users.noreply.github.com>
2025-06-25 22:52:18 -0600

    backend

--- db.js
+++ db.js
@@ -1,46 +1,19 @@
-// db.js
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const config = {
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
-};
+});
 
-function connectWithRetry(attempt = 1) {
-  const conn = mysql.createConnection(config);
-  conn.connect(err => {
-    if (err) {
-      console.error(`Conexión fallida (intento ${attempt}):`, err.code);
-      setTimeout(() => connectWithRetry(attempt + 1), 2000);
-    } else {
-      console.log('Conectado a MySQL');
-    }
-  });
-  return conn;
-}
+connection.connect((err) => {
+  if (err) {
+    console.error('Error de conexión:', err);
+    return;
+  }
+  console.log('Conectado a MySQL');
+});
 
-module.exports = connectWithRetry();
-
-// //db.js
-// require('dotenv').config();
-// const mysql = require('mysql2');
-
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME
-// });
-
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error de conexión:', err);
-//     return;
-//   }
-//   console.log('Conectado a MySQL');
-// });
-
-// module.exports = connection;
+module.exports = connection;
